cee31dcaf410246027c3a043b7e1d55d0dac27e4
SandeepJeewandara <137886591+SandeepJeewandara@users.noreply.github.com>
2023-11-13 20:57:49 +0530

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.scams_ood {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.scams_ood to javafx.fxml;
+    exports com.example.scams_ood;
+}
