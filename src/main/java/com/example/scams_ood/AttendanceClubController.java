bccae3d05b653769884bc4f644800b5068c9c66c
Lahiru Chandrasekara <147237905+LahiruChandrasekara@users.noreply.github.com>
2023-11-22 00:05:48 +0530

    JavaFx attendance tracking

---
+++ src/main/java/com/example/scams_ood/AttendanceClubController.java
@@ -0,0 +1,14 @@
+package com.example.scams_ood;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.GridPane;
+
+public class AttendanceClubController {
+
+    @FXML
+    private GridPane clubGrid;
+
+    @FXML
+    private ScrollPane clubScroll;
+}
---
+++ src/main/java/com/example/scams_ood/ClubController.java
@@ -0,0 +1,19 @@
+package com.example.scams_ood;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class ClubController {
+
+
+    @FXML
+    private Button clubButton;
+
+    @FXML
+    private ImageView clubImage;
+
+    @FXML
+    private Label clubName;
+}
