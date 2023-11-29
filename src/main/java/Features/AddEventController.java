4961f19655e05cbdc532f82fafb07329b0ab1732
Lahiru Chandrasekara <147237905+LahiruChandrasekara@users.noreply.github.com>
2023-11-29 14:57:55 +0530

    variable created

--- src/main/java/Features/AddEventController.java
+++ src/main/java/Features/AddEventController.java
@@ -1,6 +1,38 @@
 package Features;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class AddEventController {
 
+    @FXML
+    private Button CreateButton;
+
+    @FXML
+    private ComboBox<?> advisorIdInput;
+
+    @FXML
+    private ComboBox<?> clubIdInput;
+
+    @FXML
+    private DatePicker eventDateInput;
+
+    @FXML
+    private TextArea eventDescriptionInput;
+
+    @FXML
+    private TextField eventIdInput;
+
+    @FXML
+    private TextField eventNameInput;
+
+    @FXML
+    private TextField eventTimeInput;
+
+    
 
 }
